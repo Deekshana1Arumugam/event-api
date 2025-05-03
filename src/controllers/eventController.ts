cdb66ea7a9c2541bb677117081ba7729c9ff5721
Deekshana A <115058948+Deekshana1Arumugam@users.noreply.github.com>
2025-05-03 12:28:18 +0530

    list event , get event by id

--- src/controllers/eventController.ts
+++ src/controllers/eventController.ts
@@ -5,15 +5,47 @@ export const createEvent = async (req: Request, res: Response) : Promise<void> =
   try {
     const { title, description, date } = req.body;
     if (!Date.parse(date)) {
-      res.status(400).json({ error: 'Invalid date' });
+      res.status(400).json({ status:400, error: 'Invalid date' });
     }
 
     const newEvent = new Event({ title, description, date });
     await newEvent.save();
-    res.status(201).json(newEvent);
+    res.status(201).json({status:201 , newEvent});
   } catch (err) {
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ status:500 , error: 'Server error' });
   }
 };
 
+export const getEvents = async (_req: Request, res: Response): Promise<void> => {
+    try {
+      const events = await Event.find();
+
+      if (events.length === 0) {
+        res.status(404).json({ status: 404 , error: 'No events found' });
+      }else{
+        res.status(200).json({status:200 , events});;
+      }
+  
+    } catch (err) {
+      res.status(500).json({ status:500, error: 'Server error' });
+    }
+  };
+  
+  
+  export const getEventById = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const event = await Event.findById(req.params.id);
+  
+      if (!event) {
+        res.status(404).json({ status: 404, error: 'Event not found' });
+      }else{
+        res.status(200).json({status: 200, event});
+      } 
+      
+    } catch (err) {
+      res.status(500).json({ status: 500, error: 'Server error' });
+    }
+  };
+  
+  
 
--- src/routes/eventRoutes.ts
+++ src/routes/eventRoutes.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import {
   createEvent,
+  getEvents,
+  getEventById,
  
 } from '../controllers/eventController';
 
 const router = express.Router();
 
 router.post('/events', createEvent);
+router.get('/events', getEvents);
+router.get('/events/:id', getEventById);
 
 export default router;
